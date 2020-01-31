9c436d454c3263e0854a4bef13f2f7b7c82d09d8
Viraj.Jage <viraj@safexpay.com>
2020-01-31 18:38:37 +0530

    Initial commit

---
+++ app/src/test/java/com/virajjage/abl_test_viraj_jage/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.virajjage.abl_test_viraj_jage
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
